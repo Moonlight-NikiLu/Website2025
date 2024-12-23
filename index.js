6fc50c8d2e1e4fc7cbcec0c08d6ad6b52671f6f8
Moonlight-NikiLu <benjaminlu1804@gmail.com>
2024-12-23 16:34:07 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+// 提供靜態文件
+app.use(express.static("public"));
+
+// 啟動服務器
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
